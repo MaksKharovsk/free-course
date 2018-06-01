056196ae58f05cd44e3d998f5b6fd407842e4791
MaksKharovsk <MaksKharovsk@yandex.ru>
2018-06-02 01:29:12 +0300

    Решение второй задачи

---
+++ task2.js
@@ -0,0 +1,35 @@
+let d = process.argv[2],
+    m = process.argv[3],
+    y = process.argv[4];
+
+let monthNumbers = {
+  января: "0",
+  февраля: "1",
+  марта: "2",
+  апреля: "3",
+  мая: "4",
+  июня: "5",
+  июля: "6",
+  августа: "7",
+  сентября: "8",
+  октября: "9",
+  ноября: "10",
+  декабря: "11"
+};
+
+let check = (d, y) => {
+  if (d < 1 || d > 31 || y < 1 || y > 2999){
+    return true;
+  }
+  return false;
+}
+
+let calcNumbDeys = (d, m, y, monthNumbers, check) => {
+  if (check(d, y)) return "Не удовлетворяет условию";
+  
+  let date = new Date(y, monthNumbers[m], d);
+  let nextYerDate = new Date(y, 11, 31);
+  return (nextYerDate - date) / (1000 * 60 * 60 * 24);
+}
+
+process.stdout.write(calcNumbDeys(d, m, y, monthNumbers, check));

6c73c7c5c0fff2e62ea39a7b5619cecd2d541f19
MaksKharovsk <39550562+MaksKharovsk@users.noreply.github.com>
2018-06-05 11:49:30 +0300

    Update task1.js
    Добавлена проверка на соответствие входного числа его системе счисления.

--- task1.js
+++ task1.js
@@ -3,18 +3,35 @@ let i = process.argv[2],
     b = process.argv[4];
 
 let check = () => {
-  if (+a > 36 || +b > 36 || +a < 1 || +b < 1 || +i < 1 || +i > 10000) {
-    return 1;
-  }
+    if (+a > 36 || +b > 36 || +a < 2 || +b < 2 || +i < 1 || +i > 10000) {
+        return 1;
+    }
 return 0;
 }
 
-let transNum = (i, a, b, check) => {
-  if (check(i, a, b)){
-    return "Не удовлетворяет условию..."
-  }
-  let des = parseInt(i, a);
-  return des.toString(b);
+let sortArray = (a, b) => {
+    return b - a;
+}
+
+let checkBase = (sortArray) => {
+    let checkNum = i.split("").sort(sortArray);
+    if (checkNum[0] >= a){
+        return true;
+    }
+    return false;
+}
+
+let transNum = (i, a, b, check, checkBase, sortArray) => {
+    if (check(i, a, b)){
+        return "Входные данные не удовлетворяют условию..."
+    }
+
+    if (checkBase(sortArray)){
+        return "Введенное число не соответствует системе счисления..."
+    }
+
+    let des = parseInt(i, a);
+    return des.toString(b);
 }
 
 process.stdout.write(transNum(i, a, b, check));

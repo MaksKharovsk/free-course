f62fdc640499d9523068e13f2f277731b2b99022
MaksKharovsk <39550562+MaksKharovsk@users.noreply.github.com>
2018-06-07 09:01:33 +0300

    Update task1.js
    Вернул первый вариант

--- task1.js
+++ task1.js
@@ -1,37 +1,20 @@
-let i = process.argv[2],
-    a = process.argv[3],
-    b = process.argv[4];
-
-let check = () => {
-    if (+a > 36 || +b > 36 || +a < 2 || +b < 2 || +i < 1 || +i > 10000) {
-        return 1;
-    }
-return 0;
-}
-
-let sortArray = (a, b) => {
-    return b - a;
-}
-
-let checkBase = (sortArray) => {
-    let checkNum = i.split("").sort(sortArray);
-    if (checkNum[0] >= a){
-        return true;
-    }
-    return false;
-}
-
-let transNum = (i, a, b, check, checkBase, sortArray) => {
-    if (check(i, a, b)){
-        return "Входные данные не удовлетворяют условию..."
-    }
-
-    if (checkBase(sortArray)){
-        return "Введенное число не соответствует системе счисления..."
-    }
-
-    let des = parseInt(i, a);
-    return des.toString(b);
-}
-
-process.stdout.write(transNum(i, a, b, check));
++let i = process.argv[2],
++    a = process.argv[3],
++    b = process.argv[4];
++
++let check = () => {
++  if (+a > 36 || +b > 36 || +a < 1 || +b < 1 || +i < 1 || +i > 10000) {
++    return 1;
++  }
++return 0;
++}
++
++let transNum = (i, a, b, check) => {
++  if (check(i, a, b)){
++    return "Не удовлетворяет условию..."
++  }
++  let des = parseInt(i, a);
++  return des.toString(b);
++}
++
++process.stdout.write(transNum(i, a, b, check)); 

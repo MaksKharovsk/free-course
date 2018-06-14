933c256bae1cda8096785a3cffd4d9f2dbf76570
MaksKharovsk <39550562+MaksKharovsk@users.noreply.github.com>
2018-06-14 17:28:51 +0300

    Update task2.js
    Переписал вторую задачу с нуля (почти)

--- task2.js
+++ task2.js
@@ -2,34 +2,22 @@ let d = process.argv[2],
     m = process.argv[3],
     y = process.argv[4];
 
-let monthNumbers = {
-  января: "0",
-  февраля: "1",
-  марта: "2",
-  апреля: "3",
-  мая: "4",
-  июня: "5",
-  июля: "6",
-  августа: "7",
-  сентября: "8",
-  октября: "9",
-  ноября: "10",
-  декабря: "11"
-};
+function calcDays(d, m, y){
 
-let check = (d, y) => {
-  if (d < 1 || d > 31 || y < 1 || y > 2999){
-    return true;
-  }
-  return false;
-}
-
-let calcNumbDays = (check) => {
-  if (check(d, y)) return;
+  const months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
   
-  let date = new Date(y, monthNumbers[m], d);
-  let nextYerDate = new Date(y, 11, 31);
-  return (nextYerDate - date) / (1000 * 60 * 60 * 24);
+  let getMonthIndex = (m, months) => {
+    let monthIndex;
+    months.forEach(function(item, index){
+      if (m == item) monthIndex = index;
+    });
+    return monthIndex;
+  }
+  
+  let date = new Date(y, getMonthIndex(m, months), d);
+  let nextDate = new Date(y, 11, 31);  
+  
+  return (nextDate - date) / (1000 * 60 * 60 * 24);
 }
 
-process.stdout.write(calcNumbDays(check));
+process.stdout.write(calcDays(d, m, y));
